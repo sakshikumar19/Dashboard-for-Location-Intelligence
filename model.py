1dafc5913759d284b1bbe16b64f3d7adb5b265e6
sakshikumar19 <146624305+sakshikumar19@users.noreply.github.com>
2024-07-03 12:24:31 +0530

    Update model.py

--- model.py
+++ model.py
@@ -203,7 +203,7 @@ def create_competitor_plot(df, store_location):
     fig = px.scatter(df_with_store, x='Landmark Longitude', y='Landmark Latitude', color='Property Type',
                      hover_data={'Landmark Name': True, 'Landmark Latitude': True, 'Landmark Longitude': True},
                      labels={'Property Type': 'Property Type'},
-                     title="Interactive Scatter Plot of Grouped Columns")
+                     title="")
     fig.update_layout(xaxis_title="Longitude", yaxis_title="Latitude")
     return fig
 
@@ -212,6 +212,6 @@ def pie_chart(df):
     
     fig, ax = plt.subplots(figsize=(8, 8))
     ax.pie(property_counts, labels=property_counts.index, autopct='%1.1f%%', startangle=140)
-    ax.set_title('Distribution of Property Types')
+    ax.set_title("""Distribution of Property Types\n""")
     ax.axis('equal')  # Equal aspect ratio ensures that the pie is drawn as a circle.
     return fig
